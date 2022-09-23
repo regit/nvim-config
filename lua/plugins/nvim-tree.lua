510042878f2290ed7504a0cbdd8eadbf089cd7ed
Eric Leblond <el@stamus-networks.com>
2022-09-23 11:04:34 +0200

    update to latest pluging version

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,61 +11,22 @@ g.nvim_tree_width = 27
 -- g.nvim_tree_gitignore = 1
 -- g.nvim_tree_indent_markers = 1
 -- g.nvim_tree_hide_dotfiles = 1
-g.nvim_tree_git_hl = 1
-g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
-g.nvim_tree_show_icons = {
-  git = 1,
-  folders = 1,
-  files = 1
-}
-
-g.nvim_tree_icons = {
-	default = "‣ "
-}
+-- g.nvim_tree_git_hl = 1
+-- g.nvim_tree_width_allow_resize  = 1
+-- g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
+-- g.nvim_tree_show_icons = {
+--   git = 1,
+--   folders = 1,
+--   files = 1
+-- }
+-- 
+-- g.nvim_tree_icons = {
+-- 	default = "‣ "
+-- }
 
 require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  hide_dotfiles       = true,
-  tree_indent_markers = true,
-  ignore = {'.git', 'node_modules', '.cache'},
-  diagnostics         = {
-        enable = false,
-        icons = {
-          hint = "",
-          info = "",
-          warning = "",
-          error = "",
-        }
-      },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  view = {
-    width = 30,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
+  filters = {
+    dotfiles = true
   },
   git = {
     ignore = true
--- lua/plugins/paq-nvim.lua
+++ lua/plugins/paq-nvim.lua
@@ -587,9 +587,11 @@ require'diffview'.setup {
     fold_open = "",
   },
   file_panel = {
-    position = "left",            -- One of 'left', 'right', 'top', 'bottom'
-    width = 35,                   -- Only applies when position is 'left' or 'right'
-    height = 10,                  -- Only applies when position is 'top' or 'bottom'
+    win_config = {
+      position = "left",            -- One of 'left', 'right', 'top', 'bottom'
+      width = 35,                   -- Only applies when position is 'left' or 'right'
+      height = 10
+    },                  -- Only applies when position is 'top' or 'bottom'
     listing_style = "tree",       -- One of 'list' or 'tree'
     tree_options = {              -- Only applies when listing_style is 'tree'
       flatten_dirs = true,
@@ -597,16 +599,29 @@ require'diffview'.setup {
     }
   },
   file_history_panel = {
+    win_config = {
     position = "bottom",
     width = 35,
-    height = 16,
+    height = 16
+    },
     log_options = {
-      max_count = 256,      -- Limit the number of commits
-      follow = false,       -- Follow renames (only for single file)
-      all = false,          -- Include all refs under 'refs/' including HEAD
-      merges = false,       -- List only merge commits
-      no_merges = false,    -- List no merge commits
-      reverse = false,      -- List commits in reverse order
+      single_file = {
+        max_count = 256,      -- Limit the number of commits
+        follow = false,       -- Follow renames (only for single file)
+        all = false,          -- Include all refs under 'refs/' including HEAD
+        merges = false,       -- List only merge commits
+        no_merges = false,    -- List no merge commits
+        reverse = false,
+      },
+      multi_file = {
+        max_count = 128,      -- Limit the number of commits
+        follow = false,       -- Follow renames (only for single file)
+        all = false,          -- Include all refs under 'refs/' including HEAD
+        merges = false,       -- List only merge commits
+        no_merges = false,    -- List no merge commits
+        reverse = false,
+      }-- List commits in reverse order
+
     },
   },
   key_bindings = {
