f74c69af4838a81cc4d0bdaaade12ef7a5aaab44
Eric Leblond <el@stamus-networks.com>
2025-02-13 20:43:19 +0100

    add dashboard plugin

---
+++ lua/lazy-plugins/dashboard.lua
@@ -0,0 +1,13 @@
+return {
+  {
+  "nvimdev/dashboard-nvim",
+  event = "VimEnter",
+  config = function()
+    require("dashboard").setup {
+      -- config
+      -- theme = "doom",
+    }
+  end,
+  dependencies = { {"nvim-tree/nvim-web-devicons"}}
+  }
+}
