cd1eaea10e41b5c8f0b85d60f296149dfd6d614a
Eric Leblond <el@stamus-networks.com>
2025-04-06 18:45:45 +0200

    add virtual line from 0.11

--- lua/settings.lua
+++ lua/settings.lua
@@ -338,3 +338,17 @@ require("barbar").setup({
 })
 
 require("ibl").setup()
+
+
+vim.diagnostic.config({
+  -- Use the default configuration
+  virtual_lines = true,
+
+  virtual_text = false
+
+  -- Alternatively, customize specific options
+  -- virtual_lines = {
+  --  -- Only show virtual line diagnostics for the current cursor line
+  --  current_line = true,
+  -- },
+})
